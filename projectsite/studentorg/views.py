691a8eac1bd2baa40801c03c1a51dfb3da68ae0a
unknown <rjlopezvelete@gmail.com>
2026-02-24 23:31:44 +0800

    django views done(2)

--- projectsite/studentorg/views.py
+++ projectsite/studentorg/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 
 from django.views.generic.list import ListView
-from studentorg.models import Organization
+from studentorg.models import Organization, OrgMember, Student, College, Program
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from studentorg.forms import OrganizationForm
 from django.urls import reverse_lazy
@@ -33,3 +33,17 @@ class OrganizationDeleteView(DeleteView):
     model = Organization
     template_name = 'org_del.html'
     success_url = reverse_lazy('organization-list')
+
+
+
+class OrgmemberList(ListView):
+    model = OrgMember
+    context_object_name = 'orgmember'
+    template_name = 'orgmember_list.html'
+    paginate_by = 5
+
+class OrgmemberCreateView(CreateView):
+    model = Organization
+    form_class = OrganizationForm
+    template_name = 'orgmember_form.html'
+    success_url = reverse_lazy('orgmember-list')
