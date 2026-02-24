cf38af620307e76d784095fee9b25fc111bce41f
unknown <rjlopezvelete@gmail.com>
2026-02-24 22:24:07 +0800

    django views done

--- projectsite/projectsite/settings.py
+++ projectsite/projectsite/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'studentorg'
+    'studentorg',
+    'widget_tweaks',
 ]
 
 MIDDLEWARE = [
