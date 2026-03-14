531e366d3d8aaf149aae30dd8f874acf51c0b3c5
Hansyel-droid <hansmagalaman123@gmail.com>
2026-03-15 00:01:26 +0800

    auth

--- projectsite/projectsite/urls.py
+++ projectsite/projectsite/urls.py
@@ -11,18 +11,19 @@ Class-based views
     1. Add an import:  from other_app.views import Home
     2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
 Including another URLconf
-    1. Import the include() function: from django.urls import include, path
+    1. Import the include() function: from django.urls import path, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from studentorg.views import HomePageView, OrganizationList, OrganizationCreateView, OrganizationUpdateView, OrganizationDeleteView, MembersListView, MembersCreateView, MembersUpdateView, MembersDeleteView, StudentListView, StudentCreateView, StudentUpdateView, StudentDeleteView, CollegeListView, CollegeCreateView, CollegeUpdateView, CollegeDeleteView, ProgramListView, ProgramCreateView, ProgramUpdateView, ProgramDeleteView
 from studentorg import views
 
 
 
 urlpatterns = [
-    path('admin/', admin.site.urls),        
+    path('admin/', admin.site.urls),
+    path("accounts/", include("allauth.urls")), # allauth routes        
     path('', HomePageView.as_view(), name='home'),
     path('organization_list', OrganizationList.as_view(), name='organization-list'),
     path('organization_list/add', OrganizationCreateView.as_view(), name='organization-add'),
